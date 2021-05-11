2e537cd73f58dfe378a697f946658d73489b8947
Irina Kramer <irakramer@gmail.com>
2021-05-10 23:53:11 -0700

    npm install, handling new tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,7 +1,9 @@
-import { saveLikeToggle } from '../utils/api' 
+import { saveLikeToggle, saveTweet } from '../utils/api'
+import { showLoading, hideLoading, LoadingBar } from 'react-redux-loading'
 
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
 export const TOGGLE_TWEET = 'TOGGLE_TWEET';
+export const ADD_TWEET = 'ADD_TWEET';
 
 export function receiveTweets(tweets) {
     return {
@@ -10,7 +12,30 @@ export function receiveTweets(tweets) {
     }
 }
 
-function toggleTweet ( {id, authedUser, hasLiked}) {
+function addTweet(tweet) {
+    return {
+        type: ADD_TWEET,
+        tweet
+    }
+}
+
+export function handleAddTweet(text, replyingTo) {
+    return (dispatch, getState) => {
+        const { authedUser } = getState()
+
+        dispatch(showLoading())
+
+        return saveTweet({
+            text,
+            author: authedUser,
+            replyingTo,
+        })
+            .then((tweet) => dispatch(addTweet(tweet)))
+            .then(() => dispatch(hideLoading()))
+    }
+}
+
+function toggleTweet({ id, authedUser, hasLiked }) {
     return {
         type: TOGGLE_TWEET,
         id,
@@ -19,7 +44,7 @@ function toggleTweet ( {id, authedUser, hasLiked}) {
     }
 }
 
-export function handleToggleTweet (info) {
+export function handleToggleTweet(info) {
     return (dispatch) => {
         dispatch(toggleTweet(info))
 
